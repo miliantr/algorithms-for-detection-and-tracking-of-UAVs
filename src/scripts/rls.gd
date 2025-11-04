d35149a2ede8cf781df64ebd0d1091d10b3816f0
miliantr <nuiengiunewriugnwie@hotmail.com>
2025-11-04 15:43:49 +0300

    [u] add subplot

--- src/scripts/rls.gd
+++ src/scripts/rls.gd
@@ -4,9 +4,6 @@ var planeIn
 var planeBody
 var rotlock
 
-@onready var viewport := $Viewport
-@onready var camera := $Viewport/Camera3D
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,9 +11,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#var img: Image = viewport.get_texture().get_image()
-	#img.flip_y() # исправляем переворот
-	#img.save_png("user://frame.png")
 	if planeIn:
 		look_at(planeBody.global_position, Vector3.UP)
 	if not planeIn:
