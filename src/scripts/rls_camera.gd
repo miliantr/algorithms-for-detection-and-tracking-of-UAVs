26423d9abb69ac56b558718e367387b58f21397e
miliantr <goldman1476@gmail.com>
2025-12-07 03:15:42 +0300

    [u] upd t_lock

--- src/scripts/rls_camera.gd
+++
@@ -1,9 +0,0 @@
-extends Camera3D
-
-@onready var viewport := $Viewport
-@onready var camera := $Viewport/Camera3D
-
-func _process(_delta):
-	var img: Image = viewport.get_texture().get_image()
-	img.flip_y()
-	img.save_png("user://frame.png")
